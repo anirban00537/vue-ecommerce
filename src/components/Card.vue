7257b4d02552d444db2dc202f1b6c55ec3548733
Anirban Roy <anirban00537@gmail.com>
2021-09-05 23:37:31 +0600

    home page product show

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,58 @@
 <template>
   <div class="cardProduct">
-    card
+    <img
+      :src=product.imageUrl
+      class="cvr-img "
+      alt="..."
+    >
+    <div class="infoSection">
+      <h5>{{product.title}}</h5>
+      <h3 class="price">Price: 400</h3>
+      <p class="max-lines">{{product.description}}
+
+      </p>
+    </div>
   </div>
 </template>
-
-
 <script>
 export default {
-  name: "Card"
+  name: "Card",
+  props: {
+    product: Object
+  }
 };
 </script>
 <style scoped>
+.cardProduct {
+  height: 280px;
+  width: 240px;
+  margin: 10px;
+  margin-top: 40px;
+  box-shadow: 12px 8px 18px 0px rgb(225 225 225);
+  border-radius: 10px;
+}
+.imageProduct {
+}
+.infoSection {
+  margin: 10px;
+}
+.max-lines {
+  display: block;
+  text-overflow: ellipsis;
+  word-wrap: break-word;
+  overflow: hidden;
+  max-height: 1.6em;
+  line-height: 1.8em;
+  font-size: 12px;
+  margin: 0px;
+}
+.price {
+  color: rgb(55, 188, 237);
+  font-size: 17px;
+  margin: 0px;
+}
+.cvr-img {
+  max-height: 160px;
+  width: 100%;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,51 @@
 <template>
-  <div class="home">
-    home
+  <div class="container">
+    <div class="coverImage"></div>
+    <div
+      class="cstmHomeContainer"
+      v-if="products"
+    >
+      <Card
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import axios from "axios";
+import Card from "../components/Card.vue";
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    Card
+  },
+  data() {
+    return {
+      products: []
+    };
+  },
+  methods: {
+    async fetchAllProducts() {
+      const { data } = await axios.get("http://localhost:5000/api");
+      console.log(data);
+      this.products = data;
+    }
+  },
+  created() {
+    this.fetchAllProducts();
+  }
 };
 </script>
+
+<style scoped>
+.cstmHomeContainer {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+</style>>
+
+
