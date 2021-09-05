72d9c8f5a4db627d95d8b30de297e86293b27220
Anirban Roy <anirban00537@gmail.com>
2021-09-05 22:37:48 +0600

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Navbar />
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<script>
+import Navbar from "../../vue-ecommerce/src/components/Navbar.vue";
+export default {
+  components: { Navbar },
+  name: "App",
+  component: {
+    Navbar
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-cstm ">
+      <div class="container-fluid">
+        <router-link
+          to="/"
+          class="navbar-brand"
+        >Vue-commerce</router-link>
+        <div class="d-inline-flex p-2">
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            >
+            <button
+              class="btn btn-outline-success"
+              type="submit"
+            >Search</button>
+          </form>
+          <div class="cartSection">
+            <i class="fas fa-cart-plus"></i>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style>
+.navbar-cstm {
+  background-color: #fcfffe;
+  box-shadow: 3px 4px 13px 9px rgb(235, 235, 235);
+}
+.nav-link {
+  color: black !important;
+}
+.navbar-brand {
+  color: black;
+}
+
+.cartSection {
+  height: 40px;
+  width: 40px;
+  margin-top: 3px;
+  margin-left: 20px;
+  background-color: #4fc08d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+}
+.fa-cart-plus {
+  color: white;
+}
+</style>
